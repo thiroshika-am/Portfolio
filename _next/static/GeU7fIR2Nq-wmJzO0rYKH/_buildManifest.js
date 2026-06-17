19d6169416e5134f22963cec72fbaedff019cd1b
thiroshika-am <thiroshikaam@gamil.com>
2026-06-17 17:44:41 +0530

    Updates

--- _next/static/GeU7fIR2Nq-wmJzO0rYKH/_buildManifest.js
+++ _next/static/GeU7fIR2Nq-wmJzO0rYKH/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Portfolio//_next/:path+",
+        "destination": "/Portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
