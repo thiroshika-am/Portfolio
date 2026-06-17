55dfb5901503520130fc956b4d0cc465c7e1ca90
thiroshika-am <thiroshikaam@gamil.com>
2026-06-17 17:34:42 +0530

    fix github pages path

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/Portfolio",
+  assetPrefix: "/Portfolio/",
 };
 
 export default nextConfig;
